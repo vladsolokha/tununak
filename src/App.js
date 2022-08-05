1e576a7f65ea86de75831a903262bd9a2bb4d453
mbp10 <mbp10@mbp10.local>
2022-08-04 21:38:08 -0600

    added pdf to download application

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Download Application
+          Download PDF Application 
         </a>
       </header>
     </div>
